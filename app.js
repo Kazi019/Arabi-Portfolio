57ffa5795aa5295296535a1177277508b9e5251b
Kazi019 <bluesky69ster@gmail.com>
2024-08-07 17:06:56 +0600

    I delete files

--- app.js
+++
@@ -1,8 +0,0 @@
-const html = document.querySelector('html');
-const moon = document.querySelector('.moon');
-
-moon.addEventListener("click", () => {
-    html.classList.toggle('dark');
-    moon.classList.toggle('fa-moon');
-    moon.classList.toggle('fa-sun');
-});
